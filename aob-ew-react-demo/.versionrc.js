9f5f3136bed264f22c78627e92ca0258f296ddf9
Dmitriy Mirkarimov <mirkarimov@google.com>
2026-05-17 11:37:47 -0700

    chore: trying .versionrc.js

---
+++ aob-ew-react-demo/.versionrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  writerOpts: {
+    reverse: true,
+    commitsSort: false,
+    transform: (commit) => {
+      const cloned = { ...commit }
+
+      if (cloned.subject) {
+        cloned.subject = cloned.subject.replace(/\s+\(#\d+\)$/, '')
+      }
+
+      if (cloned.header) {
+        cloned.header = cloned.header.replace(/\s+\(#\d+\)$/, '')
+      }
+
+      return cloned
+    }
+  },
+  parserOpts: {
+    headerPattern: /(?:\(Merged PR \d+: \))?([a-zA-Z]+)(?:\(([\w$.\-*\s]*)\))?\!?:(.*)/,
+    breakingHeaderPattern: /(?:\(Merged PR \d+: \))?([a-zA-Z]+)(?:\(([\w$.\-*\s]*)\))?\!?!:(.*)/,
+    headerCorrespondence: [
+      'type',
+      'scope',
+      'subject'
+    ]
+  },
+  types: [
+    {
+      type: 'feat',
+      section: 'Features'
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes'
+    },
+    {
+      type: 'docs',
+      section: 'Documentation'
+    },
+    {
+      type: 'style',
+      section: 'Styling'
+    },
+    {
+      type: 'refactor',
+      section: 'Refactors'
+    },
+    {
+      type: 'perf',
+      section: 'Performance'
+    },
+    {
+      type: 'test',
+      section: 'Tests'
+    },
+    {
+      type: 'build',
+      section: 'Build System'
+    },
+    {
+      type: 'ci',
+      section: 'CI',
+      hidden: true
+    },
+    {
+      type: 'chore',
+      section: 'Chore',
+      hidden: true
+    },
+    {
+      type: 'revert',
+      section: 'Reverts'
+    }
+  ],
+  releaseCommitMessageFormat: 'chore(release): {{currentTag}} [skip ci]',
+  commitUrlFormat: 'https://github.com/mirkar/github-actions-the-complete-guide/commit/{{hash}}'
+}
